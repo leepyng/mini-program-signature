7dae25ad504d5f7dbd6e1b26f747408c4a6e856f
ChenLiping <lipingin2009@gmail.com>
2019-11-12 11:29:28 +0800

    commit
    commit

---
+++ mini-program-signature.js
@@ -0,0 +1,82 @@
+
+const app = getApp()
+
+Page({
+  	data: {
+
+  	  	img:'',
+
+        isCanvasChange:false//画板是否有数据变化，如果没有，则不能提交签名
+  	},
+
+   	/**记录开始点 */
+    bindtouchstart: function(e) {
+      	this.data.context.moveTo(e.changedTouches[0].x, e.changedTouches[0].y)
+    },
+    /**记录移动点，刷新绘制 */
+    bindtouchmove: function(e) {
+        this.data.context.lineWidth='7';
+      	this.data.context.lineTo(e.changedTouches[0].x, e.changedTouches[0].y);
+      	this.data.context.stroke();
+      	this.data.context.draw(true);
+      	this.data.context.moveTo(e.changedTouches[0].x, e.changedTouches[0].y);
+  
+    },
+    /**清空画布 */
+    bindClear: function() {
+      	// this.setCanvasBg();
+        this.data.context.clearRect(0, 0);
+        this.data.context.draw();
+
+    },
+
+    bindOk() {
+        const that=this;
+        this.setCanvasAreaInfo(()=>{
+            
+            that.data.context.draw(true , wx.canvasToTempFilePath({
+              x: 0,
+              y: 0,
+              width: that.data.width,
+              height: that.data.height,
+              destWidth: that.data.width ,
+              destHeight: that.data.height ,
+              fileType: 'png',
+              quality:1,
+              canvasId: 'signatureCanvas',
+              success(res) {
+                console.log(res);
+                that.setData({
+                  img:res.tempFilePath
+                })
+              },
+              fail(res) {
+                 console.log(res);
+              }
+            }))
+        });
+     
+    },
+
+    setCanvasAreaInfo(callBack){
+        let query = wx.createSelectorQuery();
+        const that = this;
+        query.select('#signatureCanvas').boundingClientRect(function (rect) {
+           
+            that.setData({
+                width:rect.width||0,
+                height:rect.height||0
+            })
+            callBack();
+        }).exec();
+    },
+
+  	onLoad(){
+  		  const that=this;
+  	    const context = wx.createCanvasContext('signatureCanvas');
+      	this.setData({
+  	      	context: context
+  	    })
+  	}
+
+})
